663c15c06e1bb758f76935fe3da994ad1b7e22ab
Max <pyccto@gmail.com>
2023-10-07 17:32:14 +0600

    Jump Molodez

--- src/main/java/test/testplagin/Testplagin.java
+++ src/main/java/test/testplagin/Testplagin.java
@@ -3,12 +3,14 @@ package test.testplagin;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
+
 public final class Testplagin extends JavaPlugin {
 
     @Override
     public void onEnable() {
         Logger log = getLogger();
         log.info("CAXAPOK");
+        getServer().getPluginManager().registerEvents(new EventsListener(), this);
 
     }
 
